2885fed29dff253c8aff6b73c5c0dd65e6acc349
Serstensio <92586829+Serstensio@users.noreply.github.com>
2021-10-15 21:57:38 +0200

    Update and rename c.lua to b.lua

---
+++ b.lua
@@ -0,0 +1,13 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 77 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.scr = [[
+dfg d j dfg d j|
+jJjhg gfd|
+dfg d ghj g|
+jklklklkjhj|
+dfg d ghj g|
+jklklklkjhj|
+jkllll lzlkl|
+lll l lzlkj ]]
