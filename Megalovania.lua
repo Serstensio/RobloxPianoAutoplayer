7c9ddbcd427c92ff90afa4ab9237990620d6397e
Serstensio <92586829+Serstensio@users.noreply.github.com>
2021-10-15 15:31:57 +0200

    Update and rename megalovania.lua to Megalovania.lua

--- Megalovania.lua
+++ Megalovania.lua
@@ -1,6 +1,6 @@
 shared.stop = true -- stops the player at any time if true
 -- CONFIG:
-shared.ftime = 280 -- time in seconds for the song to finish (extended by |)
+shared.ftime = 30 -- time in seconds for the song to finish (extended by |)
 shared.delay = nil -- delay overides the ftime
 shared.scr = [[
 g s O i a P P O u i s P O P s u i i i g s O i i o O P s P g s f S s g s O i a P P O u i s P O P s u i i i g s O i i o O P s P g s f S s g s O i a P P O u i s P O P s u i i i g s O i i o O P s P g s f S s
