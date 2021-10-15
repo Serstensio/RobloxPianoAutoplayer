234cb698dd643c0a920d0fcca67d851f85e28485
Serstensio <92586829+Serstensio@users.noreply.github.com>
2021-10-15 21:58:15 +0200

    Update b.lua

--- b.lua
+++ b.lua
@@ -1,6 +1,6 @@
 shared.stop = true -- stops the player at any time if true
 -- CONFIG:
-shared.ftime = 77 -- time in seconds for the song to finish (extended by |)
+shared.ftime = 50 -- time in seconds for the song to finish (extended by |)
 shared.delay = nil -- delay overides the ftime
 shared.scr = [[
 dfg d j dfg d j|
